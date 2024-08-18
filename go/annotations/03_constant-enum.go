43575c8e27cf0efe87b1609fb13ba6d3589ae497
truongtop1 <pattlactroi@gmail.com>
2024-08-18 23:19:21 +0700

    feat(go): add annotations syntax

---
+++ go/annotations/03_constant-enum.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const a = 10
+
+// iota start at 0 and increment by 1
+const (
+	red = iota
+	green
+	blue
+)
+
+// custom iota
+const (
+	_ = 1 << (10 * iota) // _ignore the first value don't want use
+	KB
+	MB
+	GB
+)
+
+func main() {
+	const i int16 = 16
+	fmt.Println(i)
+
+	// shadow constant
+	const a = 12
+	fmt.Println(a)
+
+	fmt.Println(red, green, blue)
+
+	fmt.Println(KB, MB, GB)
+}
